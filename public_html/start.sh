1a767704d953bc9118a7496d58106132d1a36e15
Patryk Janicki <kontakt.janicki@gmail.com>
2019-06-08 18:00:40 +0200

    Add xhtml file stylesheet

--- public_html/start.sh
+++ public_html/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 java -jar saxon9he.jar document.xml document-additional.xsl -o:out/document-additional.xml
+java -jar saxon9he.jar out/document-additional.xml document-xhtml.xsl -o:out/document-xhtml.html
